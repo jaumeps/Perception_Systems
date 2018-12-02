9685b382c9b9a3b38639ffe1ca68cf1932a217f2
Jaume Pregonas Sarra <jaume.pregonas@gmail.com>
2018-12-02 10:59:44 +0100

    cvtColor, GaussianBlur, HoughCircles

--- webcam_circles/src/circle_detector.cpp
+++ webcam_circles/src/circle_detector.cpp
@@ -1,5 +1,5 @@
 
-//OpenCV 
+//OpenCV
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
@@ -9,93 +9,138 @@
 #include <cstdlib>
 #include <vector>
 
+using namespace std;
+using namespace cv;
+
 //constants
 const int GAUSSIAN_BLUR_SIZE = 7;
-const double GAUSSIAN_BLUR_SIGMA = 2; 
+const double GAUSSIAN_BLUR_SIGMA = 2;
 const double CANNY_EDGE_TH = 150;
 const double HOUGH_ACCUM_RESOLUTION = 2;
 const double MIN_CIRCLE_DIST = 40;
 const double HOUGH_ACCUM_TH = 70;
 const int MIN_RADIUS = 20;
-const int MAX_RADIUS = 100;
+const int MAX_RADIUS = 30;
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    cv::VideoCapture camera; //OpenCV video capture object
-    cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
-    cv::Mat gray_image;
-    std::vector<cv::Vec3f> circles;
-    cv::Point center;
+    VideoCapture camera;  //OpenCV video capture object
+    Mat image;            //OpenCV image object
+	  int cam_id;           //camera id . Associated to device number in /dev/videoX
+    Mat gray_image;
+    vector<Vec3f> circles;
+    Point center;
     int radius;
-    
+
 	//check user args
 	switch(argc)
 	{
 		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;  
-			break; 
+			cam_id = 0;
+			break;
 		case 2: //an argument is provided. Get it and set cam_id
 			cam_id = atoi(argv[1]);
-			break; 
-		default: 
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
-			std::cout << "EXIT program." << std::endl; 
-			break; 
+			break;
+		default:
+			cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << endl;
+			cout << "EXIT program." << endl;
+			break;
 	}
-	
-	//advertising to the user 
-	std::cout << "Opening video device " << cam_id << std::endl;
+
+	//advertising to the user
+	cout << "Opening video device " << cam_id << endl;
 
     //open the video stream and make sure it's opened
-    if( !camera.open(cam_id) ) 
-	{
-        std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
+    if( !camera.open(cam_id) )
+    {
+        cout << "Error opening the camera. May be invalid device id. EXIT program." << endl;
         return -1;
     }
 
     //Process loop. Capture and point feature extraction. User can quit pressing a key
     while(1)
-	{
+    {
 		//Read image and check it. Blocking call up to a new image arrives from camera.
-        if(!camera.read(image)) 
-		{
-            std::cout << "No image" << std::endl;
-            cv::waitKey();
+        if(!camera.read(image))
+        {
+            cout << "No image" << endl;
+            waitKey();
         }
-        		
+
     //**************** Find circles in the image ****************************
-        
+
         //clear previous circles
         circles.clear();
 
-        // If input image is RGB, convert it to gray 
-        cv::cvtColor(image, gray_image, CV_BGR2GRAY);
+        // If input image is RGB, convert it to gray
+        cvtColor(image, gray_image, CV_BGR2GRAY);
+        /*
+        void cvtColor(InputArray src, OutputArray dst, int code, int dstCn=0 )
+
+        PARAMETERS:
+          src – input image: 8-bit unsigned, 16-bit unsigned ( CV_16UC... ), or single-precision floating-point.
+          dst – output image of the same size and depth as src.
+          code – color space conversion code (see the description below).
+          dstCn – number of channels in the destination image; if the parameter is 0, the number of the channels is derived automatically from src and code .
+
+        The conventional ranges for R, G, and B channel values are:
+              0 to 255 for CV_8U images
+              0 to 65535 for CV_16U images
+              0 to 1 for CV_32F images
+        */
 
         //Reduce the noise so we avoid false circle detection
-        cv::GaussianBlur( gray_image, gray_image, cv::Size(GAUSSIAN_BLUR_SIZE, GAUSSIAN_BLUR_SIZE), GAUSSIAN_BLUR_SIGMA );
+        GaussianBlur( gray_image, gray_image, Size(GAUSSIAN_BLUR_SIZE, GAUSSIAN_BLUR_SIZE), GAUSSIAN_BLUR_SIGMA );
+        /*
+        void GaussianBlur(InputArray src, OutputArray dst, Size ksize, double sigmaX, double sigmaY=0, int borderType=BORDER_DEFAULT )
+
+        PARAMETERS:
+          src - input image; the image can have any number of channels, which are processed independently, but the depth should be CV_8U, CV_16U, CV_16S, CV_32F or CV_64F.
+          dst - output image of the same size and type as src.
+          ksize - Gaussian kernel size. ksize.width and ksize.height can differ but they both must be positive and odd. Or, they can be zeroÃ¢Â€Â™s and then they are computed from sigma*.
+          sigmaX - Gaussian kernel standard deviation in X direction.
+          sigmaY - Gaussian kernel standard deviation in Y direction; if sigmaY is zero, it is set to be equal to sigmaX, if both sigmas are zeros, they are computed from ksize.width and ksize.height , respectively; to fully control the result regardless of possible future modifications of all this semantics, it is recommended to specify all of ksize, sigmaX, and sigmaY.
+          borderType - pixel extrapolation method.
+
+        */
 
         //Apply the Hough Transform to find the circles
-        cv::HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS );
-        
-        //draw circles on the image      
+        HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS );
+        /*
+        void HoughCircles(InputArray image, OutputArray circles, int method, double dp, double minDist, double param1=100, double param2=100, int minRadius=0, int maxRadius=0 )
+
+        PARAMETERS:
+          image – 8-bit, single-channel, grayscale input image.
+          circles – Output vector of found circles. Each vector is encoded as a 3-element floating-point vector (x, y, radius) .
+          circle_storage – In C function this is a memory storage that will contain the output sequence of found circles.
+          method – Detection method to use. Currently, the only implemented method is CV_HOUGH_GRADIENT , which is basically 21HT , described in [Yuen90].
+          dp – Inverse ratio of the accumulator resolution to the image resolution. For example, if dp=1 , the accumulator has the same resolution as the input image. If dp=2 , the accumulator has half as big width and height.
+          minDist – Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
+          param1 – First method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny() edge detector (the lower one is twice smaller).
+          param2 – Second method-specific parameter. In case of CV_HOUGH_GRADIENT , it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
+          minRadius – Minimum circle radius.
+          maxRadius – Maximum circle radius.
+        */
+
+
+        //draw circles on the image
         for(unsigned int ii = 0; ii < circles.size(); ii++ )
         {
             if ( circles[ii][0] != -1 )
             {
-                    center = cv::Point(cvRound(circles[ii][0]), cvRound(circles[ii][1]));
+                    center = Point(cvRound(circles[ii][0]), cvRound(circles[ii][1]));
                     radius = cvRound(circles[ii][2]);
-                    cv::circle(image, center, 5, cv::Scalar(0,255,0), -1, 8, 0 );// circle center in green
-                    cv::circle(image, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );// circle perimeter in red
+                    circle(image, center, 5, Scalar(0,255,0), -1, 8, 0 );     // circle center in green
+                    circle(image, center, radius, Scalar(0,0,255), 3, 8, 0 ); // circle perimeter in red
             }
-        }      
-        
+        }
+
     //********************************************************************
-    
+
         //show image
-        cv::imshow("Output Window", image);
+        imshow("Output Window", image);
 
 		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
-        if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
-    }   
+        if( (unsigned char)(waitKey(30) & 0xff) == 'q' ) break;
+    }
 }
--- webcam_color_detector/src/color_detector.cpp
+++ webcam_color_detector/src/color_detector.cpp
@@ -1,5 +1,4 @@
-
-//OpenCV 
+//OpenCV
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
@@ -10,9 +9,12 @@
 #include <cstdlib>
 #include <vector>
 
+using namespace std;
+using namespace cv;
+
 //constants
 const int GAUSSIAN_BLUR_SIZE = 7;
-const double GAUSSIAN_BLUR_SIGMA = 2; 
+const double GAUSSIAN_BLUR_SIGMA = 2;
 const double CANNY_EDGE_TH = 150;
 const double HOUGH_ACCUM_RESOLUTION = 2;
 const double MIN_CIRCLE_DIST = 40;
@@ -20,11 +22,11 @@ const double HOUGH_ACCUM_TH = 70;
 const int MIN_RADIUS = 20;
 const int MAX_RADIUS = 100;
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     cv::VideoCapture camera; 	//OpenCV video capture object
     cv::Mat image; 		//OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
+	  int cam_id; //camera id . Associated to device number in /dev/videoX
     cv::Mat gray_image;
     cv::Mat imageHSV;
     cv::Mat imageThreshHigh;
@@ -35,27 +37,27 @@ int main(int argc, char *argv[])
     std::vector<cv::Vec3f> circles;
     cv::Point center;
     int radius;
-    
+
 	//check user args
 	switch(argc)
 	{
 		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;  
-			break; 
+			cam_id = 0;
+			break;
 		case 2: //an argument is provided. Get it and set cam_id
 			cam_id = atoi(argv[1]);
-			break; 
-		default: 
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
-			std::cout << "EXIT program." << std::endl; 
-			break; 
+			break;
+		default:
+			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
+			std::cout << "EXIT program." << std::endl;
+			break;
 	}
-	
-	//advertising to the user 
+
+	//advertising to the user
 	std::cout << "Opening video device " << cam_id << std::endl;
 
     //open the video stream and make sure it's opened
-    if( !camera.open(cam_id) ) 
+    if( !camera.open(cam_id) )
 	{
         std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
         return -1;
@@ -65,18 +67,18 @@ int main(int argc, char *argv[])
     while(1)
 	{
 	//Read image and check it. Blocking call up to a new image arrives from camera.
-        if(!camera.read(image)) 
+        if(!camera.read(image))
 		{
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
-        
+
         //clear previous circles
         circles.clear();
 
 	//NEW
 	cv::cvtColor(image, imageHSV, CV_BGR2HSV);
-	
+
 	cv::inRange(imageHSV, cv::Scalar(0,155,155), cv::Scalar(18,255,255), imageThreshLow);
 	cv::inRange(imageHSV, cv::Scalar(165,155,155), cv::Scalar(179,255,255), imageThreshHigh);
 
@@ -92,11 +94,11 @@ int main(int argc, char *argv[])
 	cv::HoughCircles(imageThresh, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS);
 
 	for (int i = 0; i < circles.size(); i++) {
-			
+
 		 std::cout << "ball position x = " << circles[i][0] << ", y = " << circles[i][1] << ", radius = " << circles[i][2] << "\n";				cv::circle(image, cv::Point((int)circles[i][0], circles[i][1]), 3, cv::Scalar(0, 255, 0), CV_FILLED);
-		 cv::circle(image, cv::Point((int)circles[i][0], (int)circles[i][1]), (int)circles[i][2], cv::Scalar(0, 0, 255),3);			
-	} 
-	
+		 cv::circle(image, cv::Point((int)circles[i][0], (int)circles[i][1]), (int)circles[i][2], cv::Scalar(0, 0, 255),3);
+	}
+
 	/*
 	cv::namedWindow("image", CV_WINDOW_AUTOSIZE);
 	cv::namedWindow("imageThresh", CV_WINDOW_AUTOSIZE);
@@ -106,7 +108,7 @@ int main(int argc, char *argv[])
 	cv::imshow("imageThresh", imageThresh);
         cv::imshow("Output Window", image);
 
-	/*	
+	/*
 	cv::imshow("imageHSV", imageHSV);
 	cv::imshow("gray_image", gray_image);
 	cv::imshow("imagethreshlow", imageThreshLow);
@@ -116,5 +118,5 @@ int main(int argc, char *argv[])
 
 	//Waits 30 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
         if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
-    }   
+    }
 }
